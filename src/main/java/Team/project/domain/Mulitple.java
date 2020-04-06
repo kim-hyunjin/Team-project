c6a3c1451d453192ea5c5fd8340020de5eda10a1
hongseongjun90 <hongseongjun90@gmail.com>
2020-04-06 18:07:29 +0900

    [add]/Domain Class, Answer, Multiple, Question

---
+++ src/main/java/Team/project/domain/Mulitple.java
@@ -0,0 +1,24 @@
+package Team.project.domain;
+
+public class Mulitple {
+
+  private int no;
+  private String content;
+
+  public int getNo() {
+    return no;
+  }
+
+  public void setNo(int no) {
+    this.no = no;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+}
---
+++ src/main/java/Team/project/domain/Question.java
@@ -0,0 +1,54 @@
+package Team.project.domain;
+
+import java.sql.Date;
+
+public class Question {
+
+  private String title;
+  private String content;
+  private String filePath;
+  private Date deadline;
+  private Date createDate;
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public String getFilePath() {
+    return filePath;
+  }
+
+  public void setFilePath(String filePath) {
+    this.filePath = filePath;
+  }
+
+  public Date getDeadline() {
+    return deadline;
+  }
+
+  public void setDeadline(Date deadline) {
+    this.deadline = deadline;
+  }
+
+  public Date getCreateDate() {
+    return createDate;
+  }
+
+  public void setCreateDate(Date createDate) {
+    this.createDate = createDate;
+  }
+
+
+}

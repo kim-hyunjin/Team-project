1df61656deaa6359bb902826658b79762d1fc639
shinji-sub <wltjq2006@naver.com>
2020-04-26 14:42:58 +0900

    [mod]게시판 접근시 파라미터가 없어 RoomUserFilter애서 classNo값을 받지 못하는 에러 해결

--- src/main/java/Team/project/filter/RoomUserFilter.java
+++ src/main/java/Team/project/filter/RoomUserFilter.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import Team.project.domain.Clazz;
 import Team.project.domain.User;
 import Team.project.service.ClazzMemberService;
 
@@ -24,15 +25,26 @@ public class RoomUserFilter implements Filter {
     HttpServletRequest req = (HttpServletRequest) request;
     HttpServletResponse res = (HttpServletResponse) response;
     User user = (User) req.getSession().getAttribute("loginUser");
-    if(user == null  ) {
+    if(user == null) {
       res.setStatus(403);
       res.sendRedirect("/Team-project/app/auth/form");
       return;
     }
     int userNo = user.getUserNo();
     System.out.println("userNo ===> " + userNo);
-    int classNo = Integer.parseInt(req.getParameter("no"));
+    int classNo = 0;
+    try {
+    classNo = Integer.parseInt(req.getParameter("no"));
+    } catch(Exception e) {
+      Clazz clazz = (Clazz) req.getSession().getAttribute("clazzNow");
+      classNo = clazz.getClassNo();
+    }
     System.out.println("classNo ===> " + classNo);
+    if(classNo == 0) {
+      res.setStatus(403);
+      res.sendRedirect("/Team-project/app/clazz/list");
+      return;
+    }
     try {
       if (memberService.get(userNo, classNo) != null) {
         chain.doFilter(req, res);

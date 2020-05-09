6ee6ff76ad25b51ec50381dedf32e8f57e153055
chaejinho <asdzxc9395@naver.com>
2020-05-09 17:02:44 +0900

    [add] Calendar API 적용

--- src/main/java/Team/project/web/CalendarController.java
+++ src/main/java/Team/project/web/CalendarController.java
@@ -1,17 +1,10 @@
 package Team.project.web;
 
-import java.util.List;
-import java.util.Random;
-import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import Team.project.domain.Clazz;
-import Team.project.domain.ClazzMember;
-import Team.project.domain.User;
+
 import Team.project.service.ClazzMemberService;
 import Team.project.service.ClazzService;
 
